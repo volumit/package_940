eff43972d1f70e262aeed530a9e64fefe22891a3
volumit <vo_lumit@gmx.de>
2022-02-17 20:07:14 +0100

    short double handling

--- gcc/gcc/ada/gcc-interface/misc.c
+++ gcc/gcc/ada/gcc-interface/misc.c
@@ -354,8 +354,8 @@ gnat_init (void)
 {
   /* Do little here, most of the standard declarations are set up after the
      front-end has been run.  Use the same `char' as C for Interfaces.C.  */
-  build_common_tree_nodes (flag_signed_char);
-
+  /* added parameter for tricore port to flag short_double false*/
+  build_common_tree_nodes (flag_signed_char,false);
   /* In Ada, we use an unsigned 8-bit type for the default boolean type.  */
   boolean_type_node = make_unsigned_type (8);
   TREE_SET_CODE (boolean_type_node, BOOLEAN_TYPE);
