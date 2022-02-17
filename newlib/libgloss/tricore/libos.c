fb90dd1362087df877e05482905ceb09f1dd786b
volumit <vo_lumit@gmx.de>
2022-02-17 20:08:04 +0100

    removed clk and added posix dummies

--- newlib/libgloss/tricore/libos.c
+++ newlib/libgloss/tricore/libos.c
@@ -3,8 +3,8 @@
 #include<errno.h>
 #include<stdio.h>
 #include<sys/stat.h>
-
-#define FREQ 100000000	/* clock rate of processor in Hz */
+#include<sys/time.h>
+#include<sys/times.h>
 
 int __attribute__ ((weak)) _my_errno;
 __attribute__ ((weak)) int *
@@ -76,12 +76,99 @@ int fstat (int fd, struct stat *buf)
 	return 0;
 };
 
+int symlink (const char *path1, const char *path2)
+{
+  errno = ENOSYS;
+  return -1;
+}
 
+int fchmod (int __fd, mode_t __mode)
+{
+  errno = ENOSYS;
+  return -1;
+}
+
+ssize_t
+readlink (const char *path, char *buf, size_t bufsiz)
+{
+	  errno = ENOSYS;
+	  return -1;
+}
+
+int
+closedir (struct DIR *dirp)
+{
+  errno = ENOSYS;
+  return -1;
+}
+
+struct DIR *
+opendir (const char *dirname)
+{
+  errno = ENOSYS;
+  return NULL;
+}
+
+struct dirent *
+readdir (struct DIR *dirp)
+{
+  errno = ENOSYS;
+  return NULL;
+}
+
+
+/*
+ * return 0 and baselined tv, user has to override the value
+ */
 
 int gettimeofday (struct timeval *tv, void *tz)
 {
-	abort();
+	tv->tv_sec=0;
+	tv->tv_usec=0;
+	return 0;
 };
+
+char *
+getcwd (pt, size)
+     char *pt;
+     size_t size;
+{
+  errno = ENOSYS;
+  return (char *) NULL;
+}
+
+#define LINUX_LINK_MAX	127
+long int
+pathconf (path, name)
+     const char *path;
+     int name;
+{
+  errno = ENOSYS;
+  return LINUX_LINK_MAX;
+}
+
+int
+chmod (const char *path, mode_t mode)
+{
+  errno = ENOSYS;
+  return -1;
+}
+
+int
+chdir (const char *path)
+{
+  errno = ENOSYS;
+  return -1;
+}
+
+int
+mkdir (const char *path, mode_t mode)
+{
+  errno = ENOSYS;
+  return -1;
+}
+
+
 #endif
 
 #if 0
